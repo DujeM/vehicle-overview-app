6eaf45d8532816fc8472c802a96bf8219ca9f9c8
Duje Marusic <dujemarusic@gmail.com>
2024-02-20 11:40:20 +0100

    Routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/auth/login/login.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   {
     path: 'portal',
     loadChildren: () =>
       import('./views/portal/portal.module').then((m) => m.PortalModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
   },
 ];
 
